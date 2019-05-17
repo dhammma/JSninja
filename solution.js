f1ed6af86db536ea7827f9a9e36ced327775c906
Dmitrii Salenko <dmitrii.salenko@protonmail.com>
2019-05-17 14:07:05 +0300

    just a test of empty function

--- solution.js
+++ solution.js
@@ -4,44 +4,7 @@
  */
 
 function findWord(text, matrix) {
-  var width = matrix[0].length;
-  var height = matrix.length;
-  var length = text.length;
-
-  var matchedHD; // horizontal direct
-  var matchedHR; // horizontal reverse
-  var matchedVD; // vertical direct
-  var matchedVR; // vertical reverse
-
-  var i, j;
-
-  for (j = 0; j < height; j++) {
-    matchedHD = 0;
-    matchedHR = 0;
-    for (i = 0; i < width; i++) {
-      matchedHD = matrix[j][i] === text[matchedHD] ? matchedHD + 1 : 0;
-      matchedHR =
-        matrix[j][i] === text[length - 1 - matchedHR] ? matchedHR + 1 : 0;
-
-      if (matchedHD === length || matchedHR === length) {
-        return true;
-      }
-    }
-  }
-
-  for (i = 0; i < width; i++) {
-    matchedVD = 0;
-    matchedVR = 0;
-    for (j = 0; j < height; j++) {
-      matchedVD = matrix[j][i] === text[matchedVD] ? matchedVD + 1 : 0;
-      matchedVR =
-        matrix[j][i] === text[length - 1 - matchedVR] ? matchedVR + 1 : 0;
-
-      if (matchedVD === length || matchedVR === length) {
-        return true;
-      }
-    }
-  }
+  
 
   return false;
 }
